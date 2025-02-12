5be0561650b5160e6cc39ab71662344505c364c9
William Stjernström <117692509+willeStjerna@users.noreply.github.com>
2025-02-12 13:43:58 +0100

    Update test_math_operations.py.py

--- tests/test_math_operations.py.py
+++ tests/test_math_operations.py.py
@@ -1,9 +1,4 @@
-# test_math_operations.py
 import unittest
-
-# Add the project root directory to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from src.program import add_numbers
 
 class TestMathOperations(unittest.TestCase):
@@ -12,6 +7,13 @@ class TestMathOperations(unittest.TestCase):
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(0, 0), 0)
         self.assertEqual(add_numbers(10, -5), 5)
+
+    def test_add_numbers_fail(self):
+        self.assertEqual(add_numbers(2, 3), 4)
+        self.assertEqual(add_numbers(-1, 1), 0)
+        self.assertEqual(add_numbers(0, 0), 0)
+        self.assertEqual(add_numbers(10, -5), 5)
+
 
 if __name__ == "__main__":
     unittest.main()
